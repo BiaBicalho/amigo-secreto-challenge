3d195adda5a3f9176e79594e17b9be0cd2a1f888
Bia <85128774+BiaBicalho@users.noreply.github.com>
2025-08-22 08:51:22 -0300

    add funções adicionarAmigo() e limparCampo()

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+let listaAmigos = [];
 
+function adicionarAmigo(){
+  let amigo = document.querySelector('input').value;
+
+  if (amigo  == ''){
+    alert('Por favor, insira um nome');
+  }
+  else{
+    listaAmigos.push(amigo);
+  }
+  limparCampo();
+}
+
+function limparCampo(){
+  let amigo = document.querySelector('input');
+  amigo.value = '';
+}
